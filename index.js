25de7124fa490dcf81cfaaaaa1e9a779bdfae503
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-03-09 14:01:34 +0900

    Connected Database Admin To Index.js

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+
+const dbpool = require('./database/connect/makepool');
+
+app.get('/admin/database', (req, res) => {
+  res.sendFile(__dirname + '/database/admin/master_page.html');
+});
+
+const master_query = require('./database/admin/master_query');
+const wrapped_master_query = ((master_query, pool) => ((command) => master_query))(master_query, dbpool);
+
+app.post('/api/admin/master_query', (req, res) => {
+  const command = req.body.command;
+  (async () => {
+    let query_result = await wrapped_master_query(command);
+    res.json({'data': query_result});
+  })();
+});
+
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
