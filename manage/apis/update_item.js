3c2d8584fbd7d79bb7bbe522678c4ec41398b33b
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-03-14 11:22:51 +0900

    Update update_item.js

--- manage/apis/update_item.js
+++ manage/apis/update_item.js
@@ -6,15 +6,10 @@ async function update_items(req, queries) {
             category: req.body.category,
             id: req.body.id,
         };
-        console.log('updating item');
-        console.log(params);
         let item = await queries.update_item(params);
-        console.log('updated item');
-        console.log(item);
         return {data: {id: item.item_id, name: item.item_name, content: item.item_content, category: item.category_id}, error: null};
     }
     catch (err) {
-        console.log(err);
         return {data: [], error: err};
     }
 }
