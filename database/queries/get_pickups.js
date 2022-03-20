55221fa66f8ab013ee031a5b789db8ea3c207cb8
penguin234 <56291718+penguin234@users.noreply.github.com>
2022-03-20 16:40:12 +0900

    Update get_pickups.js

--- database/queries/get_pickups.js
+++ database/queries/get_pickups.js
@@ -3,7 +3,7 @@ async function get_pickups(pool, params) {
     let res = null;
 
     try {
-        const res1 = await connection.query('SELECT P.PICKUP_ID, P.PICKUP_NAME, P.PICKUP_CONTENT, C.CATEGORY_IDS FROM PICKUPS AS P LEFT JOIN (SELECT PICKUP_ID, ARRAY_AGG(CATEGORY_ID) AS CATEGORY_IDS FROM CATEGORY_CHART GROUP BY PICKUP_ID) AS C WHERE P.PICKUP_ID = C.PICKUP_ID');
+        const res1 = await connection.query('SELECT P.PICKUP_ID, PICKUP_NAME, PICKUP_CONTENT, ARRAY_AGG(C.CATEGORY_ID) AS CATEGORY_IDS FROM PICKUPS AS P LEFT JOIN CATEGORYCHART AS C ON P.PICKUP_ID = C.PICKUP_ID GROUP BY P.PICKUP_ID');
         res = res1.rows;
     }
     finally {
